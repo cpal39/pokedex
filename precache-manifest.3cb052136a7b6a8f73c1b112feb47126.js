37a94b0814b94a3af095330b53856a6f22b61a32
Chris Paldino <cpaldino9@gmail.com>
2020-04-14 15:08:56 -0400

    Updates

---
+++ precache-manifest.3cb052136a7b6a8f73c1b112feb47126.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f9f13bc008bfaedf562bd322e131f32",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "12d55647559df59ea6e5",
+    "url": "/pokedex/static/css/main.e3d414ac.chunk.css"
+  },
+  {
+    "revision": "51fc3f71885c583bf026",
+    "url": "/pokedex/static/js/2.0dc39b50.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/pokedex/static/js/2.0dc39b50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12d55647559df59ea6e5",
+    "url": "/pokedex/static/js/main.1ca458b3.chunk.js"
+  },
+  {
+    "revision": "601f8d113175821311bd",
+    "url": "/pokedex/static/js/runtime-main.0ec6b405.js"
+  },
+  {
+    "revision": "b26baf1958632e345f1ccd4d490cfe9d",
+    "url": "/pokedex/static/media/groudon.b26baf19.png"
+  },
+  {
+    "revision": "ab51ee178fe8c6518c1acc1add7515a5",
+    "url": "/pokedex/static/media/psyduck.ab51ee17.png"
+  }
+]);
